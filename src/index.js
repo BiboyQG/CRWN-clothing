273995c975052a4d433a12fab18caa7ffcb9e1b1
BiboyQG <banghao_chi@163.com>
2023-08-03 20:24:08 +0800

    Add navigation

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.scss";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
-import Home from "./routes/home/home.component";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Home />
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
