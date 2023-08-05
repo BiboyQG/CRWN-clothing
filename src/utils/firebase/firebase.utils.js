4a4bd7b4e830ab845fce77798bd609e8c2f1fc83
BiboyQG <banghao_chi@163.com>
2023-08-05 10:25:26 +0800

    Add firebase

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app'
+import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider} from 'firebase/auth'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCaVBy7LXeVwmcb4ib3JPFe95DAHOXUtZ8",
+  authDomain: "crwn-clothing-db-233cd.firebaseapp.com",
+  projectId: "crwn-clothing-db-233cd",
+  storageBucket: "crwn-clothing-db-233cd.appspot.com",
+  messagingSenderId: "587623043858",
+  appId: "1:587623043858:web:ccf73909e3b016f23e19cb",
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
