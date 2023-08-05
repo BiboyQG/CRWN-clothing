fdae0c65ff6f00816cc1cb36667003c4b882cb56
BiboyQG <banghao_chi@163.com>
2023-08-05 23:26:16 +0800

    Modify sign-in component

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,5 +1,10 @@
-import { initializeApp } from 'firebase/app'
-import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider} from 'firebase/auth'
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCaVBy7LXeVwmcb4ib3JPFe95DAHOXUtZ8",
@@ -15,8 +20,9 @@ const firebaseApp = initializeApp(firebaseConfig);
 const provider = new GoogleAuthProvider();
 
 provider.setCustomParameters({
-  prompt: "select_account"
+  prompt: "select_account",
 });
 
 export const auth = getAuth();
-export const signInWithGooglePopup = () => signInWithGooglePopup(auth, provider);
+export const signInWithGooglePopup = () =>
+  signInWithGooglePopup(auth, provider);
