dfdd10f887542f0d955674c415f8bdc678c236f9
BiboyQG <banghao_chi@163.com>
2023-08-05 23:19:42 +0800

    Setup firebase as backend

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -11,3 +11,12 @@ const firebaseConfig = {
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+  prompt: "select_account"
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithGooglePopup(auth, provider);
