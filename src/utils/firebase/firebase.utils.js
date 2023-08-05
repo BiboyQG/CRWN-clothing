5fc77767de08ae0668c800c3d64569a0429d041a
BiboyQG <banghao_chi@163.com>
2023-08-05 23:30:47 +0800

    Modify sign-in component

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -20,8 +20,8 @@ const firebaseApp = initializeApp(firebaseConfig);
 const provider = new GoogleAuthProvider();
 
 provider.setCustomParameters({
-  prompt: "select_account",
+  prompt: "select_account"
 });
 
 export const auth = getAuth();
-export const signInWithGooglePopup = () => signInWithGooglePopup(auth, provider);
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
